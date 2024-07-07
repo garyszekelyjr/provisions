a545c91b610c56221b4ecafc2107d66efc11ef32
Gary Szekely <gary.szekely.jr@gmail.com>
2024-07-07 17:03:20 -0400

    fixes

--- src/main/java/com/provisions/entities/User.java
+++ src/main/java/com/provisions/entities/User.java
@@ -1,6 +1,5 @@
 package com.provisions.entities;
 
-import com.provisions.repositories.UserRepository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
